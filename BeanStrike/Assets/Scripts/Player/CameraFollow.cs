2f1ee5364a4d8936482ceffedbee983dec8d8799
Mansour Sami <this.is.mansoursami@gmail.com>
2024-01-27 15:34:06 +0000

    Smoothing Camera
    Making the following camera more smooth.

--- BeanStrike/Assets/Scripts/Player/CameraFollow.cs
+++ BeanStrike/Assets/Scripts/Player/CameraFollow.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.125f;
+    //public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 10f;
     public Vector3 offset;
 
-    void LateUpdate () 
+    void FixedUpdate () 
     {
-        transform.position = target.position + offset;
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = desiredPosition + smoothedPosition;
+
+        transform.LookAt(target.position);
     }
 }
