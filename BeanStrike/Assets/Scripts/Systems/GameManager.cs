cb5db615f6b76ce6562b7c593aefa48ba3a77095
User1Jazz <phoenix.productions222@gmail.com>
2024-01-27 18:10:36 +0000

    Added game pause

--- BeanStrike/Assets/Scripts/Systems/GameManager.cs
+++ BeanStrike/Assets/Scripts/Systems/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int score = 0;
+    public bool gamePasused = false;
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject gameMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,16 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("escape"))
+        {
+            // Show pause menu
+            gameMenu.SetActive(!gameMenu.active);
+            pauseMenu.SetActive(!pauseMenu.active);
+
+
+            // Freeze time
+            gamePasused = !gamePasused;
+            Time.timeScale = gamePasused ? 0f : 1f; ;
+        }
     }
 }
